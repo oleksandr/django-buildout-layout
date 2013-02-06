b6dea4c383e9f33eec34249b013d6b793cec5088
Oleksandr Lobunets <alexander.lobunets@gmail.com>
2013-02-06 17:45:31 +0100

    Fixed template for nginx

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,6 +1,7 @@
 
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    url(r'^test-upload/$', TemplateView.as_view(template_name='test_upload.html'), name='upload_test'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
